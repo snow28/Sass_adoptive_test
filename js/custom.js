9a6759fe29145e5cdab92277ec24ab73e8b5220a
unknown <snow28@ukr.net>
2018-02-03 22:20:56 +0100

    first

---
+++ js/custom.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    $('.slider').slick();
+
+    $('.js-nav-menu').on('click',function(){
+        $('.body').toggleClass('body__move');
+        $('.banner').toggleClass('banner__move');
+        $('.menu').toggleClass('menu__show');
+    });
+
+
+    $(".nav-md__list-menu_js").on('click',function(){
+        $('.menu').toggleClass('menu__show');
+        if($('.menu__drop-down_ul').hasClass('menu__drop-down_ul-show')){
+            $('.menu__drop-down_ul').toggleClass('menu__drop-down_ul-show');
+        }
+    });
+
+
+    $('.man-gifts-open-js').on('click',function(){
+        $('.menu__drop-down_ul').toggleClass('menu__drop-down_ul-show');
+    });
+
+
+    $('.body').on('click', function (e) {  // как тут правильно сделать?????
+        if (!$(e.target).hasClass('nav-md__list-menu') && !$(e.target).parent().hasClass('menu__list-item')) {
+            console.log('2');
+            if($('.menu').hasClass('menu__show')){
+                $('.menu').toggleClass('menu__show');
+            }
+        }
+    });
+
+
+
+});
